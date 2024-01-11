11c03d45e06292ff47f72e9497c8030ad519c0c3
Moab Arar <moabarar@mail.tau.ac.il>
2024-01-11 10:31:16 +0200

    Changed Layout

--- util.js
+++ util.js
@@ -1,17 +1,29 @@
 let selectedArt = "boyar";
-
+let selectedSub = "toy"
 function onArtClick(event){
     newSelectedArt = event.target.parentNode.className;
     document.getElementsByClassName(selectedArt)[0].firstChild.className="artImage";
     document.getElementsByClassName(newSelectedArt)[0].firstChild.className="artImageSelected";
     selectedArt = newSelectedArt;
-    document.getElementsByClassName("toyResult")[0].firstChild.src=`data/art_inspired/${newSelectedArt}_toy.jpg`;
-    document.getElementsByClassName("catResult")[0].firstChild.src=`data/art_inspired/${newSelectedArt}_cat.jpg`;
+    document.getElementsByClassName("artResult")[0].firstChild.src=`data/art_inspired/${newSelectedArt}_${selectedSub}.jpg`;
 }
 
 function initArtSelection(){
     Array.from(document.getElementsByClassName("artImage")).forEach((e) => {e.addEventListener("click", onArtClick)});
     document.getElementsByClassName(selectedArt)[0].firstChild.className="artImageSelected";
+}
+
+function onSubjectClick(event){
+    newSelectedSub = event.target.parentNode.className;
+    document.getElementsByClassName(selectedSub)[0].firstChild.className="artRefImage";
+    document.getElementsByClassName(newSelectedSub)[0].firstChild.className="artRefImageSelected";
+    selectedSub = newSelectedSub;
+    document.getElementsByClassName("artResult")[0].firstChild.src=`data/art_inspired/${selectedArt}_${selectedSub}.jpg`;
+}
+
+function initSubjectSelection(){
+    Array.from(document.getElementsByClassName("artRefImage")).forEach((e) => {e.addEventListener("click", onSubjectClick)});
+    document.getElementsByClassName(selectedSub)[0].firstChild.className="artRefImageSelected";
 }
 
 function getOther(className)
